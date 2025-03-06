6368551676bfa1f9c7af56ee870c19526584e7fa
VINÍCIUS JOSÉ LOPES CARDOSO <24122805@dsn127m1246733.cfp127aluno.local>
2025-03-06 11:47:12 -0300

    Iniciando a segunda tela

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
@@ -0,0 +1,121 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DriveFileRenameOutline
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+    var nameState by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    listOf(Color(0xFFFF6347),
+                           Color(0xFF2707C9),
+                           Color(0xFFFF6347))
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.fitness),
+                contentDescription = "",
+                modifier = Modifier.padding(top = 32.dp)
+            )
+            Text(
+                text = stringResource(R.string.welcome),
+                fontSize = 28.sp,
+                color = Color.White,
+                fontWeight = FontWeight.ExtraBold
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp),
+                shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                    ) {
+                        Text(
+                            text = stringResource(R.string.your_name),
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                        TextField(
+                            value = nameState,
+                            onValueChange = { nameState = it},
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.DriveFileRenameOutline,
+                                    contentDescription = "",
+                                )
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp),
+                            label = {},
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = Color(0xFFE0E0E0),
+                                unfocusedContainerColor = Color(0xFFF5F5F5),
+                                disabledContainerColor = Color(0xFFF5F5F5)
+                            ),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Words
+                            )
+                        )
+                    }
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(end = 16.dp, bottom = 16.dp)
+                    ) {
+                        Text(text = stringResource(R.string.next))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
